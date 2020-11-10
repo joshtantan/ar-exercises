a8231eb5723871789bee18e9732aa13bf4cad546
Joshua Tan <josh.tan.tann@gmail.com>
2020-11-10 15:51:38 -0800

    Implement exercise 4
    Signed-off-by: Joshua Tan <josh.tan.tann@gmail.com>

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -1,9 +1,18 @@
-require_relative '../setup'
-require_relative './exercise_1'
-require_relative './exercise_2'
-require_relative './exercise_3'
+require_relative "../setup"
+require_relative "./exercise_1"
+require_relative "./exercise_2"
+require_relative "./exercise_3"
 
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Store.create(name: "Yaletown", annual_revenue: 190000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true, womens_apparel: false)
+@mens_stores.each do |mens_store|
+  pp mens_store
+end
+
+@womens_stores = Store.where(annual_revenue: 0..1000000, womens_apparel: true)
