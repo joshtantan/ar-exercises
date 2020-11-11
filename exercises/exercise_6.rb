4ccdd020232f4c99a714deb55e4f3e6a4094663f
Joshua Tan <josh.tan.tann@gmail.com>
2020-11-10 16:10:05 -0800

    Implement exercise 5
    Signed-off-by: Joshua Tan <josh.tan.tann@gmail.com>

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -1,11 +1,17 @@
-require_relative '../setup'
-require_relative './exercise_1'
-require_relative './exercise_2'
-require_relative './exercise_3'
-require_relative './exercise_4'
-require_relative './exercise_5'
+require_relative "../setup"
+require_relative "./exercise_1"
+require_relative "./exercise_2"
+require_relative "./exercise_3"
+require_relative "./exercise_4"
+require_relative "./exercise_5"
 
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Jeff", last_name: "Bezos", hourly_rate: 10)
+@store1.employees.create(first_name: "Elon", last_name: "Musk", hourly_rate: 15)
+@store2.employees.create(first_name: "Donald", last_name: "Trump", hourly_rate: 5)
+@store2.employees.create(first_name: "Barack", last_name: "Obama", hourly_rate: 20)
+pp Store.all
+# @store3 = Store.find_by(id: 3)
+# @store4 = Store
