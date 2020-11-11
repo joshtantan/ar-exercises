6a9240a390c9879b55823be477ac0fbaf0286083
Joshua Tan <josh.tan.tann@gmail.com>
2020-11-10 16:16:37 -0800

    Implement exercise 6
    Signed-off-by: Joshua Tan <josh.tan.tann@gmail.com>

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -12,6 +12,15 @@ puts "----------"
 @store1.employees.create(first_name: "Elon", last_name: "Musk", hourly_rate: 15)
 @store2.employees.create(first_name: "Donald", last_name: "Trump", hourly_rate: 5)
 @store2.employees.create(first_name: "Barack", last_name: "Obama", hourly_rate: 20)
-pp Store.all
-# @store3 = Store.find_by(id: 3)
-# @store4 = Store
+
+@store4 = Store.find_by(id: 4)
+@store4.employees.create(first_name: "Mike", last_name: "Pence", hourly_rate: 5)
+@store4.employees.create(first_name: "Ru", last_name: "Paul", hourly_rate: 20)
+
+@store5 = Store.find_by(id: 5)
+@store5.employees.create(first_name: "Gucci", last_name: "Mane", hourly_rate: 20)
+@store5.employees.create(first_name: "Carrie", last_name: "Underwood", hourly_rate: 15)
+
+@store6 = Store.find_by(id: 6)
+@store6.employees.create(first_name: "Bill", last_name: "Nye", hourly_rate: 20)
+@store6.employees.create(first_name: "Neil", last_name: "Tyson", hourly_rate: 20)
