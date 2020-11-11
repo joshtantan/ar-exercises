efaca016b79f9493dc1568966f948d2cb4553600
Joshua Tan <josh.tan.tann@gmail.com>
2020-11-10 16:03:10 -0800

    Implement exercise 5
    Signed-off-by: Joshua Tan <josh.tan.tann@gmail.com>

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -1,10 +1,12 @@
-require_relative '../setup'
-require_relative './exercise_1'
-require_relative './exercise_2'
-require_relative './exercise_3'
-require_relative './exercise_4'
+require_relative "../setup"
+require_relative "./exercise_1"
+require_relative "./exercise_2"
+require_relative "./exercise_3"
+require_relative "./exercise_4"
 
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+puts Store.sum(:annual_revenue)
+puts Store.average(:annual_revenue)
+puts Store.where("annual_revenue > ?", 1000000).size
